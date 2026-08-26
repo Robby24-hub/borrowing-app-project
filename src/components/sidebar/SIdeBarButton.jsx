153a580161273045c57bd277fa2f37d25fa03cba
Robby <jrcandano24@gmail.com>
2026-08-27 02:50:19 +0800

    code cleanup using lucide-react and map() the sidebar buttons

--- src/components/sidebar/SIdeBarButton.jsx
+++ src/components/sidebar/SIdeBarButton.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
-
-export default function SideBarButton({
-  buttonText,
-  iconWhite,
-  iconBlack,
-  isActive,
-}) {
-  const [isHovered, setIsHovered] = useState(false);
-
-  function handleSideBarButtonHover() {
-    setIsHovered(!isHovered);
-  }
-
+export default function SideBarButton({ buttonText, icon: Icon, isActive }) {
   return (
     <button
       className={`flex flex-row my-2 w-full rounded-lg py-3 px-5 cursor-pointer  transition-all duration-300 
         ${isActive ? "bg-dark text-white" : "hover:bg-primaryBlue hover:text-white"}`}
-      onMouseEnter={handleSideBarButtonHover}
-      onMouseLeave={handleSideBarButtonHover}
     >
-      <div className="relative aspect-square w-6">
-        <img
-          className={`absolute transition-all duration-300 ${isHovered || isActive ? "opacity-100" : "opacity-0"}`}
-          src={iconWhite}
-        />
-        <img
-          className={`absolute transition-all duration-300 ${isHovered || isActive ? "opacity-0" : "opacity-100"}`}
-          src={iconBlack}
-        />
-      </div>
+      <Icon size={25} />
       <span className="text-left ml-5">{buttonText}</span>
     </button>
   );
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,79 +1,67 @@
-import homeIconWhite from "/images/house.png";
-import inventoryIconWhite from "/images/boxes.png";
-import borrowIconWhite from "/images/hand-helping.png";
-import peopleIconWhite from "/images/users-round.png";
-import settingsIconWhite from "/images/settings.png";
-import logoutIconWhite from "/images/log-out.png";
-import homeIconBlack from "/images/house-black.png";
-import inventoryIconBlack from "/images/boxes-black.png";
-import borrowIconBlack from "/images/hand-helping-black.png";
-import peopleIconBlack from "/images/users-round-black.png";
-import settingsIconBlack from "/images/settings-black.png";
-import logoutIconBlack from "/images/log-out-black.png";
+import {
+  HouseIcon,
+  BoxesIcon,
+  HandHelpingIcon,
+  UsersRoundIcon,
+  SettingsIcon,
+  LogOutIcon,
+} from "lucide-react";
 import SideBarButton from "./SIdeBarButton";
 import { NavLink } from "react-router";
 
 export default function SideBar() {
+  const navLinkItems = [
+    {
+      id: crypto.randomUUID,
+      text: "Home",
+      icon: HouseIcon,
+      path: "/",
+    },
+    {
+      id: crypto.randomUUID,
+      text: "Inventory",
+      icon: BoxesIcon,
+      path: "/inventory",
+    },
+    {
+      id: crypto.randomUUID,
+      text: "Borrow",
+      icon: HandHelpingIcon,
+      path: "/borrow",
+    },
+    {
+      id: crypto.randomUUID,
+      text: "People",
+      icon: UsersRoundIcon,
+      path: "/people",
+    },
+    {
+      id: crypto.randomUUID,
+      text: "Settings",
+      icon: SettingsIcon,
+      path: "/settings",
+    },
+  ];
+
   return (
     <div className="fixed h-[calc(100vh-76px)] mt-19  w-50 flex flex-col justify-between py-5 px-3 bg-grayishWhite">
       <div className="">
-        <NavLink to="/">
-          {({ isActive }) => (
-            <SideBarButton
-              buttonText={"Home"}
-              iconWhite={homeIconWhite}
-              iconBlack={homeIconBlack}
-              isActive={isActive}
-            />
-          )}
-        </NavLink>
-        <NavLink to="/inventory">
-          {({ isActive }) => (
-            <SideBarButton
-              buttonText={"Invetory"}
-              iconWhite={inventoryIconWhite}
-              iconBlack={inventoryIconBlack}
-              isActive={isActive}
-            />
-          )}
-        </NavLink>
-        <NavLink to="/borrow">
-          {({ isActive }) => (
-            <SideBarButton
-              buttonText={"Borrow"}
-              iconWhite={borrowIconWhite}
-              iconBlack={borrowIconBlack}
-              isActive={isActive}
-            />
-          )}
-        </NavLink>
-        <NavLink to="/people">
-          {({ isActive }) => (
-            <SideBarButton
-              buttonText={"People"}
-              iconWhite={peopleIconWhite}
-              iconBlack={peopleIconBlack}
-              isActive={isActive}
-            />
-          )}
-        </NavLink>
-        <NavLink to="/settings">
-          {({ isActive }) => (
-            <SideBarButton
-              buttonText={"Settings"}
-              iconWhite={settingsIconWhite}
-              iconBlack={settingsIconBlack}
-              isActive={isActive}
-            />
-          )}
-        </NavLink>
+        {navLinkItems.map((navLinkItem) => {
+          return (
+            <NavLink to={navLinkItem.path}>
+              {({ isActive }) => (
+                <SideBarButton
+                  buttonText={navLinkItem.text}
+                  icon={navLinkItem.icon}
+                  isActive={isActive}
+                />
+              )}
+            </NavLink>
+          );
+        })}
       </div>
       <div>
-        <SideBarButton
-          buttonText={"Log Out"}
-          iconWhite={logoutIconWhite}
-          iconBlack={logoutIconBlack}
-        />
+        <SideBarButton buttonText={"Log Out"} icon={LogOutIcon} />
       </div>
     </div>
   );
