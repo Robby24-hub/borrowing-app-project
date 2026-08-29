add325252a792561f3af1fb7083257d831d40f7f
Robby <jrcandano24@gmail.com>
2026-08-30 05:41:42 +0800

    inventory controls done

--- src/pages/home/QuickActions.jsx
+++ src/pages/home/QuickActions.jsx
@@ -4,7 +4,7 @@ export default function QuickActions() {
   return (
     <div className="mb-5 ">
       <h1 className="text-xl font-semibold mb-2">Quick Actions</h1>
-      <div className="bg-white shadow-xl flex flex-row justify-between border-2 border-primaryBlue rounded-lg w-fit p-3">
+      <div className="bg-white shadow-xl flex flex-row justify-between border-2 border-primaryBlue rounded-lg w-fit py-3 px-2">
         <button className="quick-action-button">{<PlusIcon />} Add Item</button>
         <button className="quick-action-button">
           {<PlusIcon />} Add Person
--- src/pages/inventory/InventoryPage.jsx
+++ src/pages/inventory/InventoryPage.jsx
@@ -1,16 +1,31 @@
+import { ChevronDown, PlusIcon, SearchIcon } from "lucide-react";
+
 export default function InventoryPage() {
   return (
     <div>
       <div className="mb-4">
-        <h1 className="text-2xl font-semibold">Hello, Robby!</h1>
-        <p>This is what's happening with your inventory.</p>
+        <h1 className="text-2xl font-semibold">Inventory</h1>
+        <p>Manage all your items.</p>
       </div>
       <div className="mb-5">
-        <h1 className="text-xl font-semibold mb-2">Quick Actions</h1>
-        <div className="shadow-xl flex flex-row justify-between border-2 border-primaryBlue rounded-lg w-fit px-3 py-5">
-          <button className="quick-action-button">+ Add Item</button>
-          <button className="quick-action-button">+ Add Person</button>
-          <button className="quick-action-button">+ Lend Item</button>
+        <div className="shadow-xl flex flex-row justify-between border-2 border-primaryBlue rounded-lg w-full px-3 py-5">
+          <button className="inventory-button justify-start grow">
+            {<SearchIcon size={18} />} Search
+          </button>
+          <div className=" flex flex-row">
+            <button className="inventory-button">
+              All Category {<ChevronDown size={18} />}
+            </button>
+            <button className="inventory-button">
+              All Status {<ChevronDown size={18} />}
+            </button>
+            <button className="inventory-button">
+              Recently Added {<ChevronDown size={18} />}
+            </button>
+            <button className="inventory-button bg-primaryBlue text-white border-primaryBlue hover:shadow-[0_0_20px_#386aed]">
+              {<PlusIcon size={18} />} Add New Item
+            </button>
+          </div>
         </div>
       </div>
     </div>
