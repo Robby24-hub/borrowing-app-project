353cee174ea9748445fd62f2181ca04bdd59fe17
Robby <jrcandano24@gmail.com>
2026-08-30 15:14:59 +0800

    create a component called item card

--- src/pages/inventory/InventoryPage.jsx
+++ src/pages/inventory/InventoryPage.jsx
@@ -3,13 +3,14 @@ import Controls from "./Controls";
 export default function InventoryPage() {
   return (
     <div>
-      <div>
-        <div className="mb-4">
-          <h1 className="text-2xl font-semibold">Inventory</h1>
-          <p>Manage all your items.</p>
-        </div>
+      <div className="mb-4">
+        <h1 className="text-2xl font-semibold">Inventory</h1>
+        <p>Manage all your items.</p>
       </div>
       <Controls />
+      <div>
+        <h1 className="text-xl font-semibold">42 items</h1>
+      </div>
     </div>
   );
 }
