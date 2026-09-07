63f38702234ed58b9bfd6346fe8c5d0f1c67427e
Robby <jrcandano24@gmail.com>
2026-09-07 22:45:55 +0800

    sidebar changed

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -11,12 +11,7 @@ import { NavLink } from "react-router";
 
 export default function SideBar() {
   const navLinkItems = [
-    {
-      id: crypto.randomUUID(),
-      text: "Home",
-      icon: HouseIcon,
-      path: "/",
-    },
+    { id: crypto.randomUUID(), text: "Home", icon: HouseIcon, path: "/" },
     {
       id: crypto.randomUUID(),
       text: "Inventory",
@@ -44,25 +39,26 @@ export default function SideBar() {
   ];
 
   return (
-    <div className="fixed h-[calc(100vh-76px)] mt-19  w-50 flex flex-col justify-between py-5 px-3 bg-grayishWhite">
-      <div className="">
-        {navLinkItems.map((navLinkItem) => {
-          console.log(navLinkItem.id);
-          return (
-            <NavLink key={navLinkItem.id} to={navLinkItem.path}>
-              {({ isActive }) => (
-                <SideBarButton
-                  buttonText={navLinkItem.text}
-                  icon={navLinkItem.icon}
-                  isActive={isActive}
-                />
-              )}
-            </NavLink>
-          );
-        })}
+    <div className="fixed h-[calc(100vh-76px)] mt-19 w-50 flex flex-col justify-between py-5 px-3 bg-white border-r border-line">
+      <div>
+        {navLinkItems.map((navLinkItem) => (
+          <NavLink
+            key={navLinkItem.id}
+            to={navLinkItem.path}
+            end={navLinkItem.path === "/"}
+          >
+            {({ isActive }) => (
+              <SideBarButton
+                buttonText={navLinkItem.text}
+                icon={navLinkItem.icon}
+                isActive={isActive}
+              />
+            )}
+          </NavLink>
+        ))}
       </div>
       <div>
-        <SideBarButton buttonText={"Log Out"} icon={LogOutIcon} />
+        <SideBarButton buttonText="Log out" icon={LogOutIcon} />
       </div>
     </div>
   );
